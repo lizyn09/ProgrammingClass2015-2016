5eb368ef9a8a5f110ef26c90d41bb7fe9061e999
lizyn09 <lnegron@chicagotechacademy.org>
2015-09-28 11:16:17 -0500

    Update assignment4.rb

--- assignment4.rb
+++ assignment4.rb
@@ -1,11 +1,11 @@
 puts "How many hours do you work?"
-work_hours = gets.chomp.to_i
+work_hours = .to_i
 
 puts "How much do you make per hour?"
-payper_hour = gets.chomp.to_i
+pay_per_hour = .to_i
 
 puts "How many hours have you worked?"
-work_weeks = gets.chomp.to_i
+work_weeks = .to_i
 
 payment_week = work_hours * payper_hour
 
